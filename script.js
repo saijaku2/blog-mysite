d5eda5406bf33c7f6558a7f3608ef14d0267835b
saijaku2 <souta20101024@outlook.jp>
2024-10-11 14:08:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ function toggleSidebar() {
+      var sidebar = document.getElementById("sidebar");
+      var toggleButton = document.getElementById("toggleButton");
+
+      if (sidebar.classList.contains("open")) {
+        sidebar.classList.remove("open");  // サイドバーを閉じる
+        toggleButton.innerHTML = "☰";     // ボタンを「☰」に戻す
+      } else {
+        sidebar.classList.add("open");     // サイドバーを開く
+        toggleButton.innerHTML = "×";      // ボタンを「×」に変更
+      }
+    }
