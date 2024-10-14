9eb0cf4e218c2b6494a686face3327ca5559086f
saijaku2 <souta20101024@outlook.jp>
2024-10-14 22:47:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
- function toggleSidebar() {
+ window.addEventListener("load", function () {
+  setTimeout(load, 1000); // 4秒後に load 関数を実行
+  function load() {
+    document.body.classList.add("loaded"); // 'loaded' クラスを追加
+  }
+});
+
+function toggleSidebar() {
       var sidebar = document.getElementById("sidebar");
       var toggleButton = document.getElementById("toggleButton");
 
