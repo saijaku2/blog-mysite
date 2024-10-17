17499bde99065b51a2f4b2ce52f0f83bbf567230
saijaku2 <souta20101024@outlook.jp>
2024-10-17 19:39:53 +0900

    Update script.js

--- html/script.js
+++ html/script.js
@@ -10,3 +10,15 @@ function toggleSidebar() {
         toggleButton.innerHTML = "×";      // ボタンを「×」に変更
       }
     }
+window.onscroll = function() {
+    const button = document.getElementById("topButton");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+document.getElementById("topButton").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
