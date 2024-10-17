87455d933fb9f9f651a235557aba149f9c629530
saijaku2 <souta20101024@outlook.jp>
2024-10-17 21:30:01 +0900

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,34 +1,23 @@
 function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    var toggleButton = document.getElementById("toggleButton");
+      var sidebar = document.getElementById("sidebar");
+      var toggleButton = document.getElementById("toggleButton");
 
-    if (sidebar.classList.contains("open")) {
+      if (sidebar.classList.contains("open")) {
         sidebar.classList.remove("open");  // サイドバーを閉じる
         toggleButton.innerHTML = "☰";     // ボタンを「☰」に戻す
-    } else {
+      } else {
         sidebar.classList.add("open");     // サイドバーを開く
         toggleButton.innerHTML = "×";      // ボタンを「×」に変更
+      }
     }
-}
-
 window.onscroll = function() {
-    const downButton = document.getElementById("downButton");
-    const topButton = document.getElementById("topButton");
-
+    const button = document.getElementById("topButton");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-       downButton.style.display:"block"
-        topButton.style.display = "block"; // ↑ボタンを表示
+        button.style.display = "block";
     } else {
-       downButton.style.display="none"
-        topButton.style.display = "none"; // ↑ボタンを非表示
+        button.style.display = "none";
     }
 };
-
 document.getElementById("topButton").onclick = function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 };
-
-document.getElementById("downButton").onclick = function() {
-    window.scrollBy({ top: 450, behavior: 'smooth' }); // 450px スクロール
-};
-
